91ad0ef626642f41663f4c2951d048582cf496d5
Nithin <nithin@inncircles.com>
2024-10-09 11:23:44 +0530

    navbar-changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from "@angular/core";
-import { MenubarModule } from "primeng/menubar";
 
 @NgModule({
     imports: [
-        MenubarModule
     ],
     exports: [
-        MenubarModule
     ],
     providers: []
 
